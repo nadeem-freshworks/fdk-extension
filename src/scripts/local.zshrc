766fcf5d4ff461328966235393a22cee2e7653d7
Nadeem Bhati <nadeem.bhati@freshworks.com>
2024-01-16 11:35:27 +0530

    Path based Mac script

--- src/scripts/local.zshrc
+++ src/scripts/local.zshrc
@@ -5,4 +5,4 @@ export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="robbyrussell"
 
 
-export PATH="/Users/nbhati/codebase/freshdesk/fdk-extension/mynode/bin:$PATH"
+export PATH="$1/mynode/bin:$PATH"
--- src/scripts/mac.sh
+++ src/scripts/mac.sh
@@ -1,15 +1,41 @@
+echo " ____  ___       "
+echo "|     |   \  |   / "
+echo "|___  |    | |__/ "
+echo "|     |    | |  \ "
+echo "|     |__ /  |   \ "
+               
+
+curDir=`pwd` 
+printf "Please select folder for installation:\n"
+select d in ~/*; do test -n "$d" && break; echo ">>> Invalid Selection"; done
+cd "$d"
+
+nodeVersion="v18.18.2"
+npmVersion="9.8.1"
+
+dir=$(dirname "$(dirname $curDir)")
 
 
-# this script works for mac only
-# try a seective folder instead of temp folder
-#  node running check 
-cd  '/Users/nbhati/codebase/freshdesk/fdk-extension/mynode/'
+curl -o node-$nodeVersion-darwin-x64.tar.gz https://nodejs.org/dist/$nodeVersion/node-$nodeVersion-darwin-x64.tar.gz
+tar -xzf node-$nodeVersion-darwin-x64.tar.gz --strip-components=1
+source $curDir/local.zshrc $dir
 
-curl -o node-v18.12.1-darwin-x64.tar.gz https://nodejs.org/dist/v18.12.1/node-v18.12.1-darwin-x64.tar.gz
-tar -xzf node-v18.12.1-darwin-x64.tar.gz --strip-components=1
-source /Users/nbhati/codebase/freshdesk/fdk-extension/src/scripts/local.zshrc
-node -v
+nodeVer=`node -v`
+
+if [ "$nodeVersion" = "$nodeVer" ] 
+then
+    echo "Installation of node successfull!"
+else
+    echo "Installation of node failed! Please try again."
+fi
+
+npmVer=`npm -v`
+
+if [ "$npmVersion" = "$npmVer" ] 
+then
+    echo "Installation of npm successfull!"
+else
+    echo "Installation of npm failed! Please try again."
+fi
 
 npm install https://cdn.freshdev.io/fdk/latest.tgz --global
-
-
