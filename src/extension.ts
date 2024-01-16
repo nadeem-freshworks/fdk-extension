324633085a78d82602e0103cdf021415f55e2885
Nadeem Bhati <nadeem.bhati@freshworks.com>
2024-01-17 00:23:22 +0530

    platform based installation

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,57 @@
 
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as os from 'os';
 
-export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "fdk-extension" is now active!');
-	let disposable = vscode.commands.registerCommand('fdk-extension.installFdk', () => {
-		
-		vscode.window.showInformationMessage('Hello World from fdk-extension!');
 
-		const scriptPath = path.join(context.extensionPath, 'src/scripts', 'mac.sh');
-        const terminal = vscode.window.createTerminal('Install FDK');
-        terminal.show();
-        terminal.sendText(`bash ${scriptPath}`);		
+function installFDK(platform: string, context: vscode.ExtensionContext) {
+
+	if (platform === 'win32') {
+		console.log('You are on Windows.');
+
+	} else if (platform === 'darwin') {
+		console.log('You are on macOS.');
+
+		const scriptPath = path.join(context.extensionPath, 'src/scripts/mac', 'mac.sh');
+		const terminal = vscode.window.createTerminal('Install FDK');
+		terminal.show();
+		terminal.sendText(`bash ${scriptPath}`);
 		terminal.sendText(`clear`);
-		
 
-	});
+	} else {
+		console.log('Unsupported operating system.');
+	}
 
-	 disposable = vscode.commands.registerCommand('fdk-extension.loadFdk', () => { 
+}
+
+function loadFdk(platform: string, context: vscode.ExtensionContext) {
+	if (platform === 'win32') {
+		console.log('You are on Windows.');
+	} else if (platform === 'darwin') {
 		const terminal = vscode.window.createTerminal('Load  FDK');
-        terminal.show();
-		const localZshrcPath = path.join(context.extensionPath, 'src/scripts', 'local.zshrc');
-		terminal.sendText(`export PATH="/Users/nbhati/codebase/freshdesk/fdk-extension/mynode/bin:$PATH"`);
+		terminal.show();
+		const localZshrcPath = path.join(context.extensionPath, 'src/scripts/mac', 'local.zshrc');
+		terminal.sendText(`source ${localZshrcPath}`);
 		terminal.sendText(`clear`);
-		terminal.sendText(`successfully installed fdk`);
-		
+
 		const outputChannel = vscode.window.createOutputChannel('Load FDK');
 		outputChannel.appendLine('FDK loaded process completed.');
 		outputChannel.show();
+	}  else {
+		console.log('Unsupported operating system.');
+	}
+}
+export function activate(context: vscode.ExtensionContext) {
 
-		
+	const platform: string = os.platform();
+
+	console.log('Congratulations, your extension "fdk-extension" is now active!');
+	let disposable = vscode.commands.registerCommand('fdk-extension.installFdk', () => {
+		installFDK(platform, context);
+	});
+
+	disposable = vscode.commands.registerCommand('fdk-extension.loadFdk', () => {
+		loadFdk(platform, context);
 	});
 	context.subscriptions.push(disposable);
 }
